990edcdffc36755e70fa09f84b7d705182bcd564
CassiaNardoni <32914019+CassiaNardoni@users.noreply.github.com>
2025-01-20 14:24:46 -0300

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+def is_fibonacci(num):
+    a, b = 0, 1
+    while b < num:
+        a, b = b, a + b
+    return b == num or num == 0
+
+# Número a verificar
+number = int(input("Informe um número: "))
+
+if is_fibonacci(number):
+    print(f"O número {number} pertence à sequência de Fibonacci.")
+else:
+    print(f"O número {number} NÃO pertence à sequência de Fibonacci.")
+    
